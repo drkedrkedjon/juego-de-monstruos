0ee28c74dcd2ef8d04990d4b94c095943b3d1a4c
Sasa Savic Perusina <sasa@bubulazy.com>
2022-06-15 14:02:27 +0100

    Terminada la logica del juego, ahora a refactorar

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -9,6 +9,7 @@ class Personaje {
     this.aCargar = true
     this.dadosHtml = ''
     this.puntosAtaque = 0
+    this.maxEnergia = this.energia
   }
 
   numerosAleatorios() {
@@ -29,13 +30,20 @@ class Personaje {
     return numerosArray.map( num => `<div class="numero flex">${num}</div>`).join('')
   }
 
+  generarBarraHtml() {
+    const percentaje = 100 * this.energia / this.maxEnergia
+    return `
+       <div class="barra-energia" style="max-width: ${percentaje}%"></div>
+    `
+  }
+
   generarPersonajeHtml() {
     const { nombre, imagen, energia, numeroDados, puntuacionActual } = this
     return `
         <p class="nombre-persona">${nombre}</p>
         <img src="${imagen}" alt="">
         <p>Energia: <span class="energia">${energia}</span></p>
-        <div class="barra-energia"></div>
+          ${this.generarBarraHtml()}
         <div class="los-numeros flex">
           ${this.dadosHtml}
         </div>
@@ -48,17 +56,43 @@ class Personaje {
 }
 
 // *************
+function finalDeJuego() {
+  if (boni.energia > 0) {
+    document.querySelector('#batalla').innerHTML = `
+      <h2>Ha Ganado BONI</h2>
+    `
+  } else {
+    document.querySelector('#batalla').innerHTML = `
+    <h2>Han Ganado LOS MALOS</h2>
+  `
+  }
+}
+
 
 function compararAtaque() {
   elMalo.energia -= boni.puntosAtaque
   boni.energia -= elMalo.puntosAtaque
+  
+  if (boni.energia <= 0) {
+    boni.energia = 0
+    finalDeJuego()
+  } else {
+    if (elMalo.energia <= 0) {
+      elMalo.energia = 0
+      if (losMalos.length > 0) {
+        elMalo = new Personaje(data[losMalos.shift()])
+        elMalo.generarNuevosNumeros()
+      } else {
+        finalDeJuego()
+      }
+    }
+  }
 }
 
 function reducirPuntos(elArray) { 
   return elArray.reduce( (acc, num) =>  acc + num)
 }
 
-document.querySelector('#btn').addEventListener('click', atacar)
 function atacar() {
   boni.aCargar = false
   elMalo.aCargar = false
@@ -70,10 +104,11 @@ function atacar() {
 }
 
 const boni = new Personaje(data.boni)
-const elMalo = new Personaje(data[losMalos.shift()])
+let elMalo = new Personaje(data[losMalos.shift()])
 
 boni.generarNuevosNumeros()
 elMalo.generarNuevosNumeros()
+document.querySelector('#btn').addEventListener('click', atacar)
 document.querySelector('#el-bueno').innerHTML = boni.generarPersonajeHtml()
 document.querySelector('#el-malo').innerHTML = elMalo.generarPersonajeHtml()
 
